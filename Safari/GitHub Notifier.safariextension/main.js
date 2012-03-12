dbedda543da25eeb9177b7692a3ac19c7e1ebe36
Sindre Sorhus <sindresorhus@gmail.com>
2012-03-12 23:46:25 +0100

    Create Safari version of the extension

---
+++ Safari/GitHub Notifier.safariextension/main.js
@@ -0,0 +1,40 @@
+/*globals safari:true, gitHubNotifCount:true*/
+(function() {
+	'use strict';
+
+	function render( badgeText, title ) {
+		var icon = safari.extension.toolbarItems[0];
+		icon.badge = badgeText;
+		icon.toolTip = title;
+		icon.disabled = badgeText === false;
+	}
+
+	function update() {
+		gitHubNotifCount(function( count ) {
+			if ( count !== false ) {
+				render( count, 'GitHub Notifier' );
+			} else {
+				render( false, 'You have to be connected to the internet and logged into GitHub' );
+			}
+		});
+	}
+
+	var UPDATE_INTERVAL = 1000 * 60;
+
+	safari.application.addEventListener('command', function( e ) {
+		if  ( e.command === 'open-notifications' ) {
+			var win = safari.application.activeBrowserWindow;
+			var url = 'https://github.com/inbox/notifications';
+			if ( win.activeTab.url ) {
+				win.openTab().url = url;
+			} else {
+				win.activeTab.url = url;
+			}
+		}
+	});
+
+	setInterval( update, UPDATE_INTERVAL );
+
+	update();
+
+})();
